fdd14d5aecfa03f770685148114fb1e629f8fbb5
raksa <eng.raksa@gmail.com>
2019-01-09 15:19:53 +0700

    add breadcrum

---
+++ routes/breadcrumbs.php
@@ -0,0 +1,24 @@
+<?php
+
+// Home
+Breadcrumbs::for('home', function ($trail) {
+    $trail->push('Home', route('home'));
+});
+
+// Home > Clients
+Breadcrumbs::for('clients', function ($trail) {
+    $trail->parent('home');
+    $trail->push('Clients', route('clients.index'));
+});
+
+// Home > Clients > [Client]
+Breadcrumbs::for('client', function ($trail, $client) {
+    $trail->parent('clients');
+    $trail->push('client:' . $client->client_code, route('clients.show', $client->id));
+});
+
+// Home > Clients > [Client] > [Loan]
+Breadcrumbs::for('loan', function ($trail, $loan) {
+    $trail->parent('client', $loan->client);
+    $trail->push('loan:' . $loan->id, route('loans.show', $loan->id));
+});
